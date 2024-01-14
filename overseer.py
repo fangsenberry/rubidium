4da82f89117f52ed32e221aaba083d07d7909594
fangsenberry <fyl2@illinois.edu>
2024-01-14 15:08:28 -0600

    new .py file to parse and make sense of metadata created in the ruby process

---
+++ overseer.py
@@ -0,0 +1,13 @@
+'''
+This class handles parsing of the metadata of the 
+'''
+
+class Overseer():
+    def __init__(self):
+        pass
+    
+    
+'''
+This should calculate the cost for things and keep track of the per token price, so we keep track here, not other place
+should also track the mapping for status codes for ujeebu here..? or in onsearch
+'''
